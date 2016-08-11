0d6603b065acfa1dc031406ce50c10714527da41
Kyle Clemens <jkc.clemens@gmail.com>
2016-08-11 12:58:41 -0400

    fix: send the player a message instead

--- rust/src/listeners.rs
+++ rust/src/listeners.rs
@@ -23,6 +23,7 @@ impl Listeners {
 pub extern fn Java_me_kyleclemens_spongejni_rust_generated_RustyListener_joinReceived(env: *mut JNIEnv, this: jobject, event: jobject) {
   let event = unsafe { event_entity_living_humanoid_TargetHumanoidEvent::from(env, event) };
   let player = event.get_target_entity();
-  let user = unsafe { entity_living_player_User::from(player.env, player.object) };
-  println!("Rust knows that {} joined... :3", user.get_name().into_rust_string(env));
+  let user = unsafe { entity_living_player_User::from(env, player.object) };
+  let receiver = unsafe { text_channel_MessageReceiver::from(env, player.object) };
+  receiver.send_message(text_Text::of_rust(env, &format!("Welcome, {}!", user.get_name().into_rust_string(env))));
 }
