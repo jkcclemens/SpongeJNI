2c5ff3536cc1cefb2628e7de994cd04f63636ea1
Kyle Clemens <jkc.clemens@gmail.com>
2016-08-12 13:53:08 -0400

    refactor: use type aliases

--- rust/src/commands.rs
+++ rust/src/commands.rs
@@ -4,16 +4,24 @@ use generated_types::*;
 use jni::{Jni, JniUtils};
 use extensions::*;
 
+type CommandCallable = command_CommandCallable;
+type CommandContext = command_args_CommandContext;
+type CommandResult = command_CommandResult;
+type CommandSource = command_CommandSource;
+type CommandSpec = command_spec_CommandSpec;
+type MessageReceiver = text_channel_MessageReceiver;
+type Text = text_Text;
+
 pub struct Commands;
 
 impl Commands {
   pub fn register(jni: &Jni) {
     let executor = jni.generate_command_executor("me.kyleclemens.spongejni.rust.generated.HelloCommandExecutor");
-    let command = command_spec_CommandSpec::builder(jni.env)
+    let command = CommandSpec::builder(jni.env)
       .executor(executor)
       .build();
     let list = JniUtils::make_array_list(jni.env, "java/lang/String", vec!["rusty".into_java_string(jni.env)]);
-    let callable = unsafe { command_CommandCallable::from(jni.env, command.object) };
+    let callable = unsafe { CommandCallable::from(jni.env, command.object) };
     jni.get_game().get_command_manager().register_1(jni.object, callable, list);
   }
 }
@@ -24,23 +32,23 @@ pub extern fn Java_me_kyleclemens_spongejni_rust_generated_HelloCommandExecutor_
   // Here we use the static from method, which is inherently unsafe (no checks – can crash JVM)
   let (src, args, receiver) = unsafe {
     // Convert the src jobject to a CommandSource
-    let src = command_CommandSource::from(env, src);
+    let src = CommandSource::from(env, src);
     // Convert the args jobject to a CommandContext
-    let args = command_args_CommandContext::from(env, args);
+    let args = CommandContext::from(env, args);
     // Wrap the src jobject in a MessageReceiver struct
-    let receiver = text_channel_MessageReceiver::from(env, src.object);
+    let receiver = MessageReceiver::from(env, src.object);
     // Move these back into scope (Rust-specific)
     (src, args, receiver)
   };
   // Send a message to the receiver
   receiver.send_message(
     // Use the extension of_rust to ease some of the JNI quirks (use extensions::GoodText)
-    text_Text::of_rust(env, &format!(
+    Text::of_rust(env, &format!(
       "Hello, {}!",
       // Convert the Java name String to a Rust string
       src.get_name().into_rust_string(env)
     ))
   );
   // Return success
-  command_CommandResult::success(env).object
+  CommandResult::success(env).object
 }
